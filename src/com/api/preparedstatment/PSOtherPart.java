6c5adbc2a309e634d28ce838c5626360a6dd6cb9
wangfei <114542162+1228089856@users.noreply.github.com>
2024-04-13 12:59:46 +0800

    两种方式插入插入10000条数据

--- src/com/api/preparedstatment/PSOtherPart.java
+++ src/com/api/preparedstatment/PSOtherPart.java
@@ -61,4 +61,104 @@ public class PSOtherPart {
         connection.close();
 
     }
+
+    /**
+     * 使用普通的方式插入10000条数据
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    @Test
+    public void testInsert() throws ClassNotFoundException, SQLException {
+        //1. 注册驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        //2. 获取连接
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?user=root&password=wf20040704");
+
+        //3. 编写SQL语句
+        String sql = "insert into t_user(account,password,nickname) values(?,?,?);";
+
+        //4. 创建statement
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        //5. 占位符赋值
+
+        long start = System.currentTimeMillis();
+
+
+        for (int i = 0; i < 10000; i++) {
+            preparedStatement.setObject(1,"dd"+i);
+            preparedStatement.setObject(2,"dd"+i);
+            preparedStatement.setObject(3,"小王"+i);
+
+            //6. 发送SQL语句,并且获取结果
+            preparedStatement.executeUpdate();
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("普通方式插入10000条数据耗时:"+(end-start));
+
+
+        //7. 结果解析
+
+
+        //8. 关闭资源
+        preparedStatement.close();
+        connection.close();
+
+    }
+
+    /**
+     * 使用批量插入的方式插入10000条数据
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     * TODO:
+     *  1.路径后面添加?rewriteBatchedStatements=true 允许批量插入
+     *  2.insert into  values 必须写 并且语句不能添加;结束
+     *  3.不是执行语句每条，而是批量增加addBatch()
+     *  4.遍历添加完毕以后，统一批量执行 executeBatch()
+     */
+    @Test
+    public void testBatchInsert() throws ClassNotFoundException, SQLException {
+        //1. 注册驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        //2. 获取连接
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?rewriteBatchedStatements=true","root","wf20040704");
+
+        //3. 编写SQL语句
+        String sql = "insert into t_user(account,password,nickname) values(?,?,?)";
+
+        //4. 创建statement
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        //5. 占位符赋值
+
+        long start = System.currentTimeMillis();
+
+
+        for (int i = 0; i < 10000; i++) {
+            preparedStatement.setObject(1,"ddd"+i);
+            preparedStatement.setObject(2,"ddd"+i);
+            preparedStatement.setObject(3,"小王d"+i);
+
+
+            preparedStatement.addBatch();//不执行，追加到values后面
+        }
+        preparedStatement.executeBatch();//执行批量操作
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("普通方式插入10000条数据耗时:"+(end-start));
+
+
+        //7. 结果解析
+
+
+        //8. 关闭资源
+        preparedStatement.close();
+        connection.close();
+
+    }
 }
